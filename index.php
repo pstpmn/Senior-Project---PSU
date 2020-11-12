d15aac11945326a64e9ab0f1b3c05db9b613e377
Pongsathorn Pasmunee <pongsathorn.pasmunee@gmail.com>
2020-11-13 02:57:06 +0700

    My Comment to Add File

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+require 'header.php';
+?>
+
+<title>Hi</title>
+</head>
+
+<body>
+    <div id="bg_tapbar">
+        <div id="tapbar">
+            <div id="tapbar_menu">
+                <button id="btn_banner">แสนดีมณีทรัพย์</button>
+                <button id="btn_main">หน้าแรก</button>
+                <button id="btn_main">ตรวจสอบสถานะ</button>
+                <button id="btn_main">อัพสลิปการโอนเงิน</button>
+                <button id="btn_main">วิธีการจอง-ซื้อตั๋ว</button>
+            </div>
+            <div id="detail"><br><br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            จองตั๋ว - ซื้อตั๋ว ออนไลน์<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            เรือนอน สุราษฎร์ธานี - เกาะเต่า
+            </div>
+            <div id="register">
+                <div id="container">
+                    <div id="input">
+                        <br>เลือกรอบที่ต้องการออกเดินทาง
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<?php
+require 'footer.php';
+?>
