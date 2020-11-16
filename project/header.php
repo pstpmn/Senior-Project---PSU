df8b028338ffb955356b35a3aa69805dae3f89fd
FramePSU62 <74409997+FramePSU62@users.noreply.github.com>
2020-11-16 22:54:44 +0700

    Add files via upload

---
+++ project/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+    <html lang="en" dir="ltr">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=0'">
+            
+            <link rel="stylesheet" href="css/header.css">
+            <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+            
+        </head>
+         <body>
+            <nav>
+                <input type="checkbox" id="check">
+                <label for="check" class="checkbtn">
+                    <i class="fas fa-bars"></i>
+                </label>
+                    <label class="logo">แสนดีมณีทรัพย์</label>
+                        <ul>
+                            <li><a class="active" href="#">หน้าแรก</a></li>
+                            <li><a href="#">ตรวจสอบสถานะ</a></li>
+                            <li><a href="#">อัพสลิปการโอนเงิน</a></li>
+                            <li><a href="#">เช็ดอิน - เช็ดเอาท์</a></li>
+                        </ul>
+                        </nav>
+
+             </body>
+        </html>
