5982be53f5f002c6a637d6d9919ec861321ed8c6
FramePSU62 <74409997+FramePSU62@users.noreply.github.com>
2020-11-30 23:59:02 +0700

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,59 @@
+<?php include('header.php');?>
+    <section class="fullheight align-times-center img img-fixed" 
+        style="background-image: url('image/tk.png');">
+        <div class="container">
+                <div class="slogan">
+                    
+                    <h1>จองตั๋ว - ซื้อตั๋ว ออนไลน์<br>
+                    <br>เรือนอน สุราษฎร์ธานี - เกาะเต่า</h1>
+                
+                </div>
+        
+                <div class="box">
+                    <h2>เลือกวันที่ต้องการออกเดินทาง</h2> <br>   
+                        
+                    <p><b>ปลายทาง</b></p>
+                    
+                        <select name="list" size="1">
+                            <option value="1">------ปลายททาง------<br>
+                            <option value="2">สุราษฎร์ธานี - เกาะเต่า<br>
+                            <option value="3">เกาะเต่า - สุราษฎร์ธานี<br>
+                        </select></br></br>
+                    
+                    
+                    <p><b>วันที่ออกเดินทาง</b></p>
+                        <input type="date" name=""> </br></br>
+                        <center>
+                            <button type="button"class="btn btn-primary"><span>ค้นหาที่นั่ง</span></button>
+                        </center>
+                </div>
+        </div>
+    </section> 
+
+    <div class="container2">
+        <center><h2>เลือกวันที่ต้องการออกเดินทาง</h2> <br></center>   
+                        
+        <p><b>ปลายทาง</b></p>
+           <center>         
+            <select name="list" size="1" class="form-control">
+                <option value="1">------ปลายททาง------<br>
+                <option value="2">สุราษฎร์ธานี - เกาะเต่า<br>
+                <option value="3">เกาะเต่า - สุราษฎร์ธานี<br>
+            </select></br></br>
+            </center>
+        
+        <p><b>วันที่ออกเดินทาง</b></p>
+        <center>
+            <input class="form-control" type="date" name=""> </br></br>
+        </center>
+            <center>
+                <button type="button"class="btn btn-primary"><span>ค้นหาที่นั่ง</span></button>
+            </center>
+    </div>
+
+    <center>
+        <div class="call">
+            <p>Call Center : 087 - 8873162</p>
+    </center>
+    <?php include('footer.php');?>
+    
---
+++ project/status.php
@@ -0,0 +1,22 @@
+<?php include('header.php');?>
+    
+    <center>
+        </br>
+        </br>
+        </br>
+        </br>
+        <h3 class="has3">แสนดีมรีทรัพย์<br>
+        <br>จองตั๋ว - ซื้อตั๋ว ออนไลน์ เรือนอน สุราษฎร์ธานี - เกาะเต่า</h3>
+        </br>
+        </br>
+        </br>
+        </br>
+
+        <form>
+        <h4 class="has4">
+        รหัสการซื้อตั๋ว : 
+        <input  type= "text"  size="30"  name="id"  placeholder="รหัสการซื้อตั๋ว"/><br>
+        <br>
+        หมายเลขโทรศัพท์ : 
+        <input  type= "text"  size="30"  name="phone"  placeholder="หมายเลขโทรศัพท์"/></h3>
+        </form>
